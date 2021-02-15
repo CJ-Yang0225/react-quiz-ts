778f875999b56915c28b3d30cb6a709b1306183d
CJ-Yang0225 <57297482+CJ-Yang0225@users.noreply.github.com>
2021-02-15 17:00:53 +0800

    chore: remove useEffect hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 // Components
 import QuizCard from "./QuizCard";
 import QuizInfoBar from "./QuizInfoBar";
